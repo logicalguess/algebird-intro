04fc7643b9173e924a2b0eb2b0b8b0084edfd4d8
logicalguess <logicalguess@gmail.com>
2017-08-22 01:15:37 -0400

    reflection

--- src/main/scala/logicalguess/algebird/component/StateMonad.scala
+++ src/main/scala/logicalguess/algebird/component/StateMonad.scala
@@ -37,6 +37,10 @@ object StateMonad {
   def sequence[S, A](lma: List[StateMonad[S, A]]): StateMonad[S, List[A]] =
     lma.foldRight(state[S, List[A]](List[A]()))((ma, mla) => map2[S, A, List[A], List[A]](ma, mla)(_ :: _))
 
+  def chain[S, A](lma: List[StateMonad[S, A]]): StateMonad[S, A] =
+    lma.foldRight(state[S, List[A]](List[A]()))((ma, mla) => map2[S, A, List[A], List[A]](ma, mla)(_ :: _)).map(_.last)
+
+
   def traverse[S, A, B](la: List[A])(f: A => StateMonad[S, B]): StateMonad[S, List[B]] =
     la.foldRight(state[S, List[B]](List[B]()))((a, mlb) => map2(f(a), mlb)(_ :: _))
 
@@ -63,10 +67,11 @@ trait ComponentMonad[I, S, O] extends StateMonad[S, O] {
 }
 
 object Test {
+
   def main (args: Array[String] ): Unit = {
     val c = new ComponentMonad[Int, Int, Int] {
       override def logic: PartialFunction[(Int, Int), Int] = { case (i, j) => i + j }
-      override def transformer: Function[Int, Int] = identity
+      override def transformer: Function[Int, Int] = 2*_
     }
 
     var m = for {
@@ -87,8 +92,58 @@ object Test {
     val seqm = StateMonad.sequence(List(2, 3, 5).map(c.process))
     println(seqm.run(1))
 
+    val chainm = StateMonad.chain(List(2, 3, 5).map(c.process))
+    println(chainm.run(1))
+
+
     val updm: StateMonad[Int, List[Unit]] = StateMonad.sequence[Int, Unit](List(2, 3, 5).map(c.update))
     println(updm.run(1)._1)
+
+    val sum: (Int, Int, Int) => Int = _ + _ + _
+    val sumCurried: Int => Int => Int => Int = sum.curried
+
+    val f: Int => Int = sum.curried(1)(2)
+    val g: Int => Int = sum(1, 2, _: Int)
+
+    println(f(3)) //6
+    println(g(3)) //6
+
+    def factory[A](a: A): StateMonad[A => Any, Any] = {
+      type X = A => Any
+      StateMonad[X, Any] { f =>
+        val x: X = if (f.apply(a).isInstanceOf[X]) f.apply(a).asInstanceOf[X] else identity
+
+        (x, f.apply(a))
+      }
+    }
+
+    val fm = StateMonad.chain(List(2, 3, 5).map(factory))
+    println(fm.run(sum.curried)._2)
+
+    val concat: (String, String, String) => String = _ + _ + _
+    val fm1 = StateMonad.chain(List("a", "b", "c").map(factory))
+    println(fm1.run(concat.curried)._2)
+
+    object fs {
+      def comb(list: Any*): String = {
+        list.mkString
+      }
+
+      def comb1(i: Int, s1: String, s2: String): String = i + s1 + s2
+
+    }
+
+    val args = List(2, "b", "c")
+    println(fs.comb(args: _*))
+
+    import scala.reflect.runtime.universe._
+    val im = runtimeMirror(this.getClass.getClassLoader).reflect(fs)
+
+    val method = im.symbol.typeSignature.member(TermName("comb")).asMethod
+    println("comb: " + im.reflectMethod(method)(args))
+
+    val method1 = im.symbol.typeSignature.member(TermName("comb1")).asMethod
+    println("comb1: " + im.reflectMethod(method1)(args: _*))
 
   }
 }
