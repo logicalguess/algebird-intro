1097fd713456eae377a5a631a7231c6ec59f9993
logicalguess <logicalguess@gmail.com>
2017-08-21 23:16:07 -0400

    state monad sequence

--- src/main/scala/logicalguess/algebird/component/StateMonad.scala
+++ src/main/scala/logicalguess/algebird/component/StateMonad.scala
@@ -30,6 +30,16 @@ object StateMonad {
   def get[S]: StateMonad[S, S] = StateMonad { s: S => (s, s) }
   def gets[S, A](f: S => A): StateMonad[S, A] = StateMonad { s: S => (s, f(s)) }
   def modify[S](f: S => S): StateMonad[S, Unit] = StateMonad { s: S => (f(s), ()) }
+
+  def map2[S, A, B, C](ma: StateMonad[S, A], mb: StateMonad[S, B])(f: (A, B) => C): StateMonad[S, C] =
+    ma.flatMap(a => mb.map(b => f(a, b)))
+
+  def sequence[S, A](lma: List[StateMonad[S, A]]): StateMonad[S, List[A]] =
+    lma.foldRight(state[S, List[A]](List[A]()))((ma, mla) => map2[S, A, List[A], List[A]](ma, mla)(_ :: _))
+
+  def traverse[S, A, B](la: List[A])(f: A => StateMonad[S, B]): StateMonad[S, List[B]] =
+    la.foldRight(state[S, List[B]](List[B]()))((a, mlb) => map2(f(a), mlb)(_ :: _))
+
 }
 
 trait ComponentMonad[I, S, O] extends StateMonad[S, O] {
@@ -45,6 +55,10 @@ trait ComponentMonad[I, S, O] extends StateMonad[S, O] {
         (s1, transformer(s1))}
       //_ <- StateMonad.modify { s: S => logic.apply((input, s)) }
     } yield updated
+  }
+
+  def update(input: I): StateMonad[S, Unit] = {
+    StateMonad.modify { s: S => logic.apply((input, s)) }
   }
 }
 
@@ -69,6 +83,12 @@ object Test {
     })
 
     println(sm.run(1))
+
+    val seqm = StateMonad.sequence(List(2, 3, 5).map(c.process))
+    println(seqm.run(1))
+
+    val updm: StateMonad[Int, List[Unit]] = StateMonad.sequence[Int, Unit](List(2, 3, 5).map(c.update))
+    println(updm.run(1)._1)
 
   }
 }
